dd4e6a1491933039fd19e64473c6c33a0bae61eb
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2023-04-04 08:07:23 -0300

    Update jokenpo.py

--- jokenpo.py
+++ jokenpo.py
@@ -1,70 +1,83 @@
-from random import choice
-from time import sleep
+def main():
+    vitorias, empates, derrotas = 0, 0, 0
+    while True:
+        mostar_opcoes()
 
-answer = 'S'
-win = draw = loss = 0
+        jogada = int(input("Qual a sua jogada? "))
+        if 0 <= jogada <= 2:
+            resultado = jogar(jogada)
+            # Atualizando variáveis de desempenho
+            if resultado == "v":
+                vitorias += 1
+            elif resultado == "e":
+                empates += 1
+            else:
+                derrotas += 1
+        else:
+            print("JOGADA INVÁLIDA")
+        
+        resposta = str(input("\nQuer continuar? [S/N] ")).strip().upper()[0]
+        if resposta != "S":
+            desempenho(vitorias, empates, derrotas)
+            break
 
-while answer == "S":
-    print('''SUAS OPÇÕES: 
-[ 0 ] Pedra
-[ 1 ] Papel
-[ 2 ] Tesoura''')
 
-    player = int(input("Qual a sua jogada? "))
+def mostar_opcoes():
+    print('''\nSUAS OPÇÕES: 
+    [ 0 ] Pedra
+    [ 1 ] Papel
+    [ 2 ] Tesoura''')
 
-    if player in [0, 1, 2]:
-        if player == 0:
-            player = 'Pedra'
-        elif player == 1:
-            player = "Papel"
-        elif player == 2:
-            player = "Tesoura"
 
-        computer = choice(['Pedra', 'Papel', 'Tesoura'])
+def jogar(jogada_usuario):
+    from random import randint
+    from time import sleep
 
-        print("JO")
-        sleep(1)
-        print("KEN")
-        sleep(1)
-        print("PO!!!")
+    opcoes = ['Pedra', 'Papel', 'Tesoura']
+    jogada_computador = randint(0, 2)
 
-        print("=" * 25)
-        print(f"Computador jogou {computer}")
-        print(f"Jogador jogou {player}")
-        print("=" * 25)
+    print("JO")
+    sleep(1)
+    print("KEN")
+    sleep(1)
+    print("PO!!!")
 
-        # Jogador vence
-        if player == 'Pedra' and computer == 'Tesoura':
-            print("JOGADOR VENCE")
-            win += 1
-        elif player == 'Papel' and computer == 'Pedra':
-            print("JOGADOR VENCE")
-            win += 1
-        elif player == 'Tesoura' and computer == 'Papel':
-            print("JOGADOR VENCE")
-            win += 1
+    print(f"Computador jogou {opcoes[jogada_computador]}")
+    print(f"Jogador jogou {opcoes[jogada_usuario]}")
 
-        # Empate
-        if player == computer:
-            print("EMPATE")
-            draw += 1
+    # Jogador vence
+    if jogada_usuario == 0 and jogada_computador == 2: # Pedra x Tesoura
+        print("JOGADOR VENCE")
+        return "v"
+    elif jogada_usuario == 1 and jogada_computador == 0: # Papel x Pedra
+        print("JOGADOR VENCE")
+        return "v"
+    elif jogada_usuario == 2 and jogada_computador == 1: # Tesoura x Papel
+        print("JOGADOR VENCE")
+        return "v"
 
-        # Computador vence
-        if computer == 'Pedra' and player == 'Tesoura':
-            print("COMPUTADOR VENCE")
-            loss += 1
-        elif computer == 'Papel' and player == 'Pedra':
-            print("COMPUTADOR VENCE")
-            loss += 1
-        elif computer == 'Tesoura' and player == 'Papel':
-            print("COMPUTADOR VENCE")
-            loss += 1
-    else:
-        print("JOGADA INVÁLIDA")
-    
-    answer = str(input("Quer continuar? [S/N] ")).strip().upper()[0]
+    # Empate
+    if jogada_usuario == jogada_computador:
+        print("EMPATE")
+        return "e"
 
-print("Seu desempenho:")
-print(f"{win} Vitórias")
-print(f"{draw} Empates")
-print(f"{loss} Derrotas")
+    # Computador vence
+    if jogada_computador == 0 and jogada_usuario == 2: # Pedra x Tesoura
+        print("COMPUTADOR VENCE")
+        return "d"
+    elif jogada_computador == 1 and jogada_usuario == 0: # Papel x Pedra
+        print("COMPUTADOR VENCE")
+        return "d"
+    elif jogada_computador == 2 and jogada_usuario == 1: # Tesoura x Papel
+        print("COMPUTADOR VENCE")
+        return "d"
+
+
+def desempenho(tot_vitorias, tot_empates, tot_derrotas):
+    print("\nSeu desempenho:")
+    print(f"{tot_vitorias} Vitórias")
+    print(f"{tot_empates} Empates")
+    print(f"{tot_derrotas} Derrotas\n")
+
+
+main()
